51f3125c63f96ff6392128ea951e04029b10ad07
FraChr <empynia@gmail.com>
2026-07-03 13:20:51 +0200

    main home view

--- src/Views/HomeView.vue
+++ src/Views/HomeView.vue
@@ -1,9 +1,11 @@
 ﻿<script setup lang="ts">
 
+import Certificates from "@/Components/Certificates.vue";
 </script>
 
 <template>
   <h1>TESTING HOME VIEW</h1>
+  <Certificates/>
 </template>
 
 <style scoped>
