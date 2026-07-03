c949ef394123a72ffa618d89d3c469d636a4c7b6
FraChr <empynia@gmail.com>
2026-07-03 15:02:06 +0200

    type for certificate upload form

---
+++ src/Types/CertificateForm.ts
@@ -0,0 +1,8 @@
+﻿export interface CertificateForm {
+    type: string;
+    number: number | null;
+    notifiedBody: string;
+    issueDate: string | null;
+    expiryDate: string | null;
+    file: File | null;
+}
