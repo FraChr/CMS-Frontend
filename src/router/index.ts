520280dd5ade7b0ddd471812c48a6f4d8874d802
FraChr <empynia@gmail.com>
2026-07-03 15:00:32 +0200

    added route path to certificate upload page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from "@/Views/HomeView.vue";
+import CertificateUploadForm from "@/Components/CertificateUploadForm.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       name: 'HomeView',
       path: '/',
       component: HomeView,
+    },
+    {
+      name: 'Upload',
+      path: '/certificate/upload',
+      component: CertificateUploadForm
     }
   ],
 })
