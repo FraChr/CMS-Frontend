5496de46c68031cf1d118e26e96283c36fe94fad
FraChr <empynia@gmail.com>
2026-07-03 13:29:01 +0200

    axios client config

---
+++ src/Services/Api.ts
@@ -0,0 +1,13 @@
+﻿import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:5044',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    paramsSerializer: {
+        indexes: null
+    }
+});
+
+export default apiClient;
