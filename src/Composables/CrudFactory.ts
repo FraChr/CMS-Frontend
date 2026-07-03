9fdf5acfcccfd43645765b9eb723e82ea362c590
FraChr <empynia@gmail.com>
2026-07-03 13:26:38 +0200

    typed CRUD composable bindings

---
+++ src/Composables/CrudFactory.ts
@@ -0,0 +1,8 @@
+﻿import {useCrud} from "@/Composables/UseCrud.ts";
+import type {Certificate} from "@/Types/Certificate.ts";
+
+
+export const crudFactory = {
+    useCertificate: () => useCrud<Certificate>('certificates')
+
+}
