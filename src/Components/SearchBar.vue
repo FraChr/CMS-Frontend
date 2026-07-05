8d6b64a273da02644f96cbb7f901a8ec59a116f3
FraChr <empynia@gmail.com>
2026-07-05 13:24:39 +0200

    added searchbar component

---
+++ src/Components/SearchBar.vue
@@ -0,0 +1,21 @@
+﻿<script setup lang="ts">
+  const model = defineModel<string>({default: ''});
+  const emit = defineEmits<{search: [value: string]}>();
+
+  const search = () => {
+    emit('search', model.value);
+  }
+</script>
+
+<template>
+  <input
+  v-model="model"
+  placeholder="search certificate number"
+  @keyup.enter="search"/>
+
+  <button @click="search">Search</button>
+</template>
+
+<style scoped>
+
+</style>
