89cf6b9964384f95ee94511fe7d3a3de9709c240
FraChr <empynia@gmail.com>
2026-07-05 13:25:48 +0200

    added types to curd service

--- src/Services/CrudServices.ts
+++ src/Services/CrudServices.ts
@@ -1,10 +1,14 @@
 ﻿import apiClient from './Api.ts';
 
-export const createCrudServices = (route: string) => ({
+export const createCrudServices = <T>(route: string) => ({
     getList: (params: Record<string, any> = {}) =>
-        apiClient.get(`/${route}`, { params }),
-    getFile: (id: number) => apiClient.get(`/${route}/${id}/file`, {
+        apiClient.get<T[]>(`/${route}`, { params }),
+
+    getFile: (id: number) =>
+        apiClient.get(`/${route}/${id}/file`, {
         responseType: "blob"
     }),
+    getTypes: () => apiClient.get<string[]>(`/${route}/types`),
+
     postFile: (formData: FormData) => apiClient.post(`/${route}/upload`, formData)
 })
