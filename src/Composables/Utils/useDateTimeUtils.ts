31d604377c209610a2e58b5c6fc45de20e598530
FraChr <empynia@gmail.com>
2026-07-05 13:23:30 +0200

    added time convert utils

---
+++ src/Composables/Utils/useDateTimeUtils.ts
@@ -0,0 +1,16 @@
+﻿export function useDateTimeUtils() {
+    const convertUtcToDate = (utcDateTime: string) => {
+        let date = new Date(utcDateTime);
+        return date.toLocaleDateString();
+    }
+
+    const convertUtcToDateTime = (utcDateTime: string) => {
+        let date = new Date(utcDateTime);
+        return date.toLocaleString();
+    }
+
+    return {
+        convertUtcToDate,
+        convertUtcToDateTime,
+    }
+}
